70629431b4a85245b9afa0be525bb92d49ff4008
laalaguer <laalaguer@gmail.com>
2016-10-04 09:06:02 +0200

    clean code

--- python/samples/jsontomovie.py
+++ python/samples/jsontomovie.py
@@ -27,9 +27,8 @@ def get_single_ffmpeg_cake(fcpcake):
     return {'uid': cake_uid, 'layers': layers}
 
 
-if __name__ == "__main__":
-    grouped_fcp_cakes = jsonhelper.json_file_to_jsonobj('sample.json')
-
+def convert_fcp_to_ffmpeg(grouped_fcp_cakes):
+    ''' convert fcp cakes into ffmpeg cakes '''
     ffmpeg_cakes = []  # The ffmpeg cakes ready to be put on the oven of ffmpeg to cook
 
     for each_cake_hash in grouped_fcp_cakes.keys():
@@ -37,6 +36,13 @@ if __name__ == "__main__":
         for each_single_cake in splitted_fcp_cakes:
             ffmpeg_cakes.append(get_single_ffmpeg_cake(each_single_cake))
 
+    return ffmpeg_cakes
+
+
+def get_render_commands(ffmpeg_cakes):
+    ''' Input: ffmpeg_cakes description, in a list. final_concat_name shall contain .mp4
+    Output: Command lines that can be executed by bash/sh
+    '''
     render_commands = []  # Command line to execute one by one finally.
     movie_chunk_names = []  # Generated movie file names on the hard disk.
     for each_ffmpeg_cake in ffmpeg_cakes:
@@ -44,37 +50,38 @@ if __name__ == "__main__":
         render_commands.append(bake.generate_single_render_command_line(each_ffmpeg_cake, output_file_name))
         movie_chunk_names.append(output_file_name)
 
-    render_commands.append(bake.generate_concat_command_line(movie_chunk_names, 'mixslice_movie.mp4'))
-    # with open('sample.json', 'r') as f:
-    #     json_string = ''.join(f.readlines())
-    #     jsonobj = json.loads(json_string)
+    return render_commands
 
-    #     fcpjson = jsonobj
-    #     movie_parts = []  # container of temp generated movie file names
-    #     render_commands = []  # command line to execute one by one finally
-    #     for idx, each_cake in enumerate(fcpjson):
-    #         output_file_name = 'test-part-%d.mp4' % idx
-    #         ffmpeg_cake = get_single_ffmpeg_cake(each_cake)
-    #         render_commands.append(bake.generate_single_render_command_line(ffmpeg_cake, output_file_name))
-    #         movie_parts.append(output_file_name)
 
-    #     render_commands.append(bake.generate_concat_command_line(movie_parts, 'mixslice_movie.mp4'))
+def render_ffmpeg_cakes(render_commands):
+    ''' Real operating system execution to render ffmpeg cake '''
+    import subprocess
+    for each in render_commands:  # bake each ffmpeg cake into movie chunk
+        print each
+        print '-----------------'
+        subprocess.call(each, shell=True)
 
+
+if __name__ == "__main__":
+    grouped_fcp_cakes = jsonhelper.json_file_to_jsonobj('sample.json')
+
+    ffmpeg_cakes = convert_fcp_to_ffmpeg(grouped_fcp_cakes)
+
+    render_commands = get_render_commands(ffmpeg_cakes)
+
+    # Fix: r2 or r3 not related to the actual file name
     linked_render_commands = []
     for each in render_commands:
         temp = each.replace(' r2 ', ' WP_20140830_14_39_10_Pro.mp4 ').replace(' r3 ', ' WP_20140830_15_13_44_Pro.mp4 ')
         linked_render_commands.append(temp)
 
     render_commands = linked_render_commands
-    import subprocess
-    for each in render_commands[0:-1]:  # bake each ffmpeg cake into movie chunk
-        print each
-        print '-----------------'
-        subprocess.call(each, shell=True)
+    # Fix end.
 
+    render_ffmpeg_cakes(render_commands)
     # join the movie parts together into a final movie.
-    commandline = []
-    commandline.append('/bin/bash')
-    commandline.append('-c')
-    commandline.append(render_commands[-1])
-    child = subprocess.Popen(commandline)
+    # commandline = []
+    # commandline.append('/bin/bash')
+    # commandline.append('-c')
+    # commandline.append(render_commands[-1])
+    # child = subprocess.Popen(commandline)
